1a73822282f77163c84522cd30b9d61fb45da61c
Mufei Li <mufeili1996@gmail.com>
2024-11-05 01:10:00 -0500

    update

--- retrieve/emb.py
+++ retrieve/emb.py
@@ -1,11 +1,19 @@
+import os
 import torch
 
 from src.config.emb import load_yaml
 
 def main(args):
-    # Check the config file for advanced settings and extensions.
+    # Modify the config file for advanced settings and extensions.
     config_file = f'configs/emb/gte-large-en-v1.5/{args.dataset}.yaml'
     config = load_yaml(config_file)
+    
+    torch.set_num_threads(config['env']['num_threads'])
+
+    if args.dataset == 'cwq':
+        input_file = os.path.join('rmanluo', 'RoG-cwq')
+    else:
+        input_file = os.path.join('ml1996', 'webqsp')
 
 if __name__ == '__main__':
     from argparse import ArgumentParser

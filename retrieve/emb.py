d925bd1591f31142d0008e7511a22508d246d3b7
Mufei Li <mufeili1996@gmail.com>
2024-11-05 00:48:18 -0500

    update

---
+++ retrieve/emb.py
@@ -0,0 +1,15 @@
+import torch
+
+def main(args):
+    # Check the config file for advanced settings and extensions.
+    config_file = f'configs/emb/gte-large-en-v1.5/{args.dataset}.yaml'
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    
+    parser = ArgumentParser('Text Embedding Pre-Computation for Retrieval')
+    parser.add_argument('-d', '--dataset', type=str, required=True, 
+                        choices=['webqsp', 'cwq'], help='Dataset name')
+    args = parser.parse_args()
+    
+    main(args)

90787544d2d228114a3c76791febefae35c09524
Mufei Li <mufeili1996@gmail.com>
2024-11-06 23:38:04 -0500

    update

--- retrieve/train.py
+++ retrieve/train.py
@@ -3,6 +3,7 @@ from src.config.retriever import load_yaml
 def main(args):
     # Modify the config file for advanced settings and extensions.
     config_file = f'configs/retriever/{args.dataset}.yaml'
+    config = load_yaml(config_file)
 
 if __name__ == '__main__':
     from argparse import ArgumentParser

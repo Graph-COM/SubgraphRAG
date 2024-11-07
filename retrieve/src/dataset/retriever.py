fa0244de53537226c7f3ea09adccb56c0414565a
Mufei Li <mufeili1996@gmail.com>
2024-11-07 14:25:49 -0500

    update

--- retrieve/src/dataset/retriever.py
+++ retrieve/src/dataset/retriever.py
@@ -2,13 +2,35 @@ import os
 import pickle
 
 class RetrieverDataset:
-    def __init__(self, config, split, skip_no_path=True):
+    def __init__(
+        self,
+        config,
+        split,
+        skip_no_path=True
+    ):
         # Load pre-processed data.
         dataset_name = config['dataset']['name']
         processed_dict_list = self._load_processed(dataset_name, split)
 
-    def _load_processed(self, dataset_name, split):
+        # Extract directed shortest paths from topic entities to answer
+        # entities or vice versa as weak supervision signals for triple scoring.
+        triple_score_dict = self._get_triple_scores(
+            dataset_name, split, processed_dict_list)
+
+    def _load_processed(
+        self,
+        dataset_name,
+        split
+    ):
         processed_file = os.path.join(
             f'data_files/{dataset_name}/processed/{split}.pkl')
         with open(processed_file, 'rb') as f:
             return pickle.load(f)
+
+    def _get_triple_scores(
+        self,
+        dataset_name,
+        split,
+        processed_dict_list
+    ):
+        pass

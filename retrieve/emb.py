6e5c15f94044f0cf871c015cfcfdcad5d862709a
Mufei Li <mufeili1996@gmail.com>
2024-11-06 22:44:52 -0500

    update

--- retrieve/emb.py
+++ retrieve/emb.py
@@ -2,8 +2,26 @@ import os
 import torch
 
 from datasets import load_dataset
+from tqdm import tqdm
 
 from src.config.emb import load_yaml
+from src.dataset.emb import EmbInferDataset
+
+def get_emb(subset, text_encoder, save_file):
+    emb_dict = dict()
+    for i in tqdm(range(len(subset))):
+        id, q_text, text_entity_list, relation_list = subset[i]
+        
+        q_emb, entity_embs, relation_embs = text_encoder(
+            q_text, text_entity_list, relation_list)
+        emb_dict_i = {
+            'q_emb': q_emb,
+            'entity_embs': entity_embs,
+            'relation_embs': relation_embs
+        }
+        emb_dict[id] = emb_dict_i
+    
+    torch.save(emb_dict, save_file)
 
 def main(args):
     # Modify the config file for advanced settings and extensions.
@@ -29,6 +47,39 @@ def main(args):
     
     save_dir = f'data_files/{args.dataset}/processed'
     os.makedirs(save_dir, exist_ok=True)
+
+    train_set = EmbInferDataset(
+        train_set,
+        entity_identifiers,
+        os.path.join(save_dir, 'train.pkl'))
+
+    val_set = EmbInferDataset(
+        val_set,
+        entity_identifiers,
+        os.path.join(save_dir, 'val.pkl'))
+
+    test_set = EmbInferDataset(
+        test_set,
+        entity_identifiers,
+        os.path.join(save_dir, 'test.pkl'),
+        skip_no_topic=False,
+        skip_no_ans=False)
+    
+    device = torch.device('cuda:0')
+    
+    text_encoder_name = config['text_encoder']['name']
+    if text_encoder_name == 'gte-large-en-v1.5':
+        from src.model.text_encoders import GTELargeEN
+        text_encoder = GTELargeEN(device)
+    else:
+        raise NotImplementedError(text_encoder_name)
+    
+    emb_save_dir = f'data_files/{args.dataset}/emb/{text_encoder_name}'
+    os.makedirs(emb_save_dir, exist_ok=True)
+    
+    get_emb(train_set, text_encoder, os.path.join(emb_save_dir, 'train.pth'))
+    get_emb(val_set, text_encoder, os.path.join(emb_save_dir, 'val.pth'))
+    get_emb(test_set, text_encoder, os.path.join(emb_save_dir, 'test.pth'))
 
 if __name__ == '__main__':
     from argparse import ArgumentParser

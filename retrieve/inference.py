9a487e1a61b55ccef70922658c5c1236ea739cd4
Mufei Li <mufeili1996@gmail.com>
2024-11-08 17:14:17 -0500

    update

--- retrieve/inference.py
+++ retrieve/inference.py
@@ -1,7 +1,26 @@
 import torch
 
+from src.dataset.retriever import RetrieverDataset
+from src.model.retriever import Retriever
+from src.setup import set_seed
+
+@torch.no_grad()
 def main(args):
-    torch.load(args.path)
+    device = torch.device(f'cuda:0')
+    
+    cpt = torch.load(args.path, map_location='cpu')
+    config = cpt['config']
+    set_seed(config['env']['seed'])
+    torch.set_num_threads(config['env']['num_threads'])
+    
+    infer_set = RetrieverDataset(
+        config=config, split='test', skip_no_path=False)
+    
+    emb_size = infer_set[0]['q_emb'].shape[-1]
+    model = Retriever(emb_size, **config['retriever']).to(device)
+    model.load_state_dict(cpt['model_state_dict'])
+    model = model.to(device)
+    model.eval()
 
 if __name__ == '__main__':
     from argparse import ArgumentParser

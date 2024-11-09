663c8d3edf0a2138558c2cb4697660d60a473c75
Mufei Li <mufeili1996@gmail.com>
2024-11-09 12:20:20 -0500

    update

--- retrieve/eval.py
+++ retrieve/eval.py
@@ -6,9 +6,11 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-d', '--dataset', type=str, required=True, 
                         choices=['webqsp', 'cwq'], help='Dataset name')
-    parser.add_argument()
+    parser.add_argument('-p', '--path', type=str, required=True,
+                        help='Path to retrieval result')
     # parser.add_argument()
-    parser.add_argument()
+    parser.add_argument('--k_list', type=str, default='50,100,200,400',
+                        help='Comma-separated list of K values for top-K recall evaluation')
     args = parser.parse_args()
     
     main(args)
